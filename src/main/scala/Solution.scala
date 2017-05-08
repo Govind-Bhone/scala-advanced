2041bea0e4533a602f0be04ebc862468a90e28bf
Govind <bhonegovind@gmail.com>
2017-05-08 17:03:23 +0530

    applicatives,monads , functors in scala

---
+++ src/main/scala/Solution.scala
@@ -0,0 +1,26 @@
+/**
+  * Created by govind.bhone on 5/8/2017.
+  */
+/*
+We can't override mutable variable but we can assign value to it .
+*/
+class Vehicle {
+  var speed: Int = 60
+
+}
+
+class Bike extends Vehicle {
+  //override var speed: Int = 100
+  speed = 100
+
+  def show() {
+    println(speed)
+  }
+}
+
+object MainObject {
+  def main(args: Array[String]) {
+    var b = new Bike()
+    b.show()
+  }
+}
